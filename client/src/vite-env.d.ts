afe419d2556cce72b056967f83800ee09e7327b0
Antirender <z487213030@gmail.com>
2025-10-11 20:53:53 -0400

    Add files via upload

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE: string;
-  readonly VITE_UNSPLASH_ACCESS_KEY: string;
+  readonly VITE_API_URL: string;
 }
 
 interface ImportMeta {
